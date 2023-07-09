29c0e6807458d551a6ac143a658e918a50e84eb2
professional.sourav <professional.sourav@gmail.com>
2023-07-09 08:18:59 +0530

    Added seleniumwire for headless scraping and created a route for that

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from bs4 import BeautifulSoup
 import json
 import logging
 import sys
+from seleniumwire import webdriver
+
+# import seleniumwire.undetected_chromedriver as uc
 
 app = Flask(__name__)
 
@@ -18,7 +21,6 @@ def hello():
 
 @app.route('/scrap', methods=['GET', 'POST'])
 def scrap():
-
     if request.form.get('headers'):
         headers = json.loads(request.form.get("headers"))
     else:
@@ -40,4 +42,57 @@ def scrap():
         "content": page.text,
         "headers": dict(page.headers),
         "status": page.status_code
+    }
+
+
+@app.route('/headless', methods=['GET', 'POST'])
+def headless():
+    # other chrome options
+    chrome_options = webdriver.ChromeOptions()
+    # chrome_options.headless = True
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--ignore-certificate-errors-spki-list')
+    chrome_options.add_argument('--ignore-ssl-errors')
+    chrome_options.add_argument("--window-size=1920,1200")
+
+    ### This blocks images and javascript requests
+    chrome_prefs = {
+        "profile.default_content_setting_values": {
+            "images": 2,
+            "javascript": 2,
+        }
+    }
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    ###
+
+    driver = webdriver.Chrome(options=chrome_options)
+
+    # url = request.form.get('url')
+
+    CUSTOM_UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
+    SEC_CH_UA = '"Google Chrome";v="112", " Not;A Brand";v="99", "Chromium";v="112"'
+    REFERER = 'https://google.com'
+
+    # print(url)
+
+    driver.get('https://www.ionos.com/')
+
+    response = ''
+
+    for request in driver.requests:
+        if request.url == "https://www.ionos.com/":
+            response = request.response
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    driver.quit()
+
+    # print(response[0].headers.as_bytes())
+
+    # return dict(response.headers)
+
+    return {
+        "content": soup.html.decode(),
+        "headers": dict(response.headers)
     }
